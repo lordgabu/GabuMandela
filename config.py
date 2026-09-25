69418e4bcac8936b53b3864ac1ce457b1f588504
Gabu <vinodsharma98176@gmail.com>
2026-09-26 00:38:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = int(os.environ.get("API_ID", "5910854772"))
 API_HASH = os.environ.get("API_HASH", "5910854772eef58bab25ec9aa78a9742")
 
 # Bot Token from @BotFather
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN") or os.getenv("BOT_TOKEN", "8762166731:AAEUa-CEhKRLsfWGDdSk4Gau_ujJbMHgcFs")
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN") or os.getenv("BOT_TOKEN", "8922066156:AAE55IQNXrwLox6aZSsn_J-TcSYpZWPpj0g")
 
 # Owner ID and Authorized Users
 OWNER_ID = int(os.environ.get("OWNER_ID", "8701689281"))
